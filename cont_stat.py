8fca4314b7fcd52cdc6f3295b13f72e3d4864b0a
elpuffin <elpuffin2@gmail.com>
2018-02-14 18:56:32 +0000

    1st working

--- cont_stat.py
+++ cont_stat.py
@@ -3,6 +3,7 @@
 import sys
 import requests
 import gzip
+import collections
 
 def get_archi():
   if len(sys.argv) == 2:
@@ -18,26 +19,48 @@ def get_archi():
       's390x']
   
     if  sys.argv[1] in opts_archi:
-      archi = sys.argv[1]
-      return archi
+      return sys.argv[1]
     else:
-      print "Argument not in:", opts_archi 
+      print "Argument \""+ sys.argv[1] +"\" not in:", opts_archi 
+      sys.exit(1)
   else:
     print "Nope"
     sys.exit(1)
 
-
 def get_contentsIndex(arc):
   cont_url = "http://ftp.uk.debian.org/debian/dists/stable/main/Contents-"+ arc + ".gz"
+  print "Downloading:"
+  print cont_url
+
   cont_get = requests.get(cont_url)
   file_archi =cont_url.split('/')[-1]
+  print "Writing to: "+ file_archi
+
   with open(file_archi, "wb") as f: f.write(cont_get.content)
   return file_archi
 
 def process_gzip(archivo):
+  print "Processing gunzip: "+ archivo
+
   with gzip.open(archivo, "rb") as gz: gz_content = gz.read()
-  return gz_content
+  return gz_content.replace(',', '\n')
 
-print process_gzip(get_contentsIndex(get_archi()))
+def sort_list(processed):
+  package_list = []
+  #for pepe in process_gzip(get_contentsIndex(get_archi())).splitlines():
+  for pepe in processed.splitlines():
+    package_list.append(pepe.split()[-1].split('/')[-1])
 
+  contador = collections.Counter(package_list)
+
+  top_ten = contador.most_common(10)
+  print "\n Printing output: \n"
+  no = 1
+  for pepe in top_ten:
+    print str(no) +" {0[0]} {0[1]}".format(pepe)
+    no = no + 1
+
+l = process_gzip(get_contentsIndex(get_archi()))
+
+sort_list(l)
 

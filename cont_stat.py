9487fe26b82d7e89999f7e38c2069a68b25b5af6
elpuffin <elpuffin2@gmail.com>
2018-02-14 21:17:11 +0000

    pep8 mods

--- cont_stat.py
+++ cont_stat.py
@@ -5,62 +5,74 @@ import requests
 import gzip
 import collections
 
+
 def get_archi():
-  if len(sys.argv) == 2:
-    opts_archi = ['amd64', 
-      'arm64', 
-      'armel', 
-      'armhf',
-      'i386', 
-      'mips',
-      'mips64el',
-      'mipsel',
-      'ppc64el',
-      's390x']
-  
-    if  sys.argv[1] in opts_archi:
-      return sys.argv[1]
+    if len(sys.argv) == 2:
+        opts_archi = ['amd64', 'arm64', 'armel', 'armhf', 'i386', 'mips',
+        'mips64el', 'mipsel', 'ppc64el', 's390x']
+
+        if sys.argv[1] in opts_archi:
+            return sys.argv[1]
+        elif sys.argv[1].startswith("udeb-"):
+            if sys.argv[1].split("udeb-")[-1] in opts_archi:
+                return sys.argv[1]
+            else:
+                print "Parameter '" + sys.argv[1] + "' not valid"
+                print "Should be one of: udeb-", opts_archi
+                sys.exit(1)
+        else:
+            print "Parameter \"" + sys.argv[1] + "\" not valid:"
+            print "Should be one of: ", opts_archi
+            sys.exit(1)
     else:
-      print "Argument \""+ sys.argv[1] +"\" not in:", opts_archi 
-      sys.exit(1)
-  else:
-    print "Nope"
-    sys.exit(1)
+        print "Nope"
+        sys.exit(1)
+
 
 def get_contentsIndex(arc):
-  cont_url = "http://ftp.uk.debian.org/debian/dists/stable/main/Contents-"+ arc + ".gz"
-  print "Downloading:"
-  print cont_url
+    url = "http://ftp.uk.debian.org/debian/dists/stable/main/Contents-"
+    cont_url = url + arc + ".gz"
 
-  cont_get = requests.get(cont_url)
-  file_archi =cont_url.split('/')[-1]
-  print "Writing to: "+ file_archi
+    print "Downloading:"
+    print cont_url
 
-  with open(file_archi, "wb") as f: f.write(cont_get.content)
-  return file_archi
+    cont_get = requests.get(cont_url)
+    file_archi = cont_url.split('/')[-1]
+    print "Writing to: " + file_archi
+
+    with open(file_archi, "wb") as f:
+        f.write(cont_get.content)
+    return file_archi
+
 
 def process_gzip(archivo):
-  print "Processing gunzip: "+ archivo
+    print "Processing gunzip: " + archivo
 
-  with gzip.open(archivo, "rb") as gz: gz_content = gz.read()
-  return gz_content.replace(',', '\n')
+    with gzip.open(archivo, "rb") as gz:
+        gz_content = gz.read()
+    return gz_content.replace(',', '\n')
+
 
 def sort_list(processed):
-  package_list = []
-  #for pepe in process_gzip(get_contentsIndex(get_archi())).splitlines():
-  for pepe in processed.splitlines():
-    package_list.append(pepe.split()[-1].split('/')[-1])
+    package_list = []
 
-  contador = collections.Counter(package_list)
+    for pepe in processed.splitlines():
+        package_list.append(pepe.split()[-1].split('/')[-1])
 
-  top_ten = contador.most_common(10)
-  print "\n Printing output: \n"
-  no = 1
-  for pepe in top_ten:
-    print str(no) +" {0[0]} {0[1]}".format(pepe)
-    no = no + 1
+    contador = collections.Counter(package_list)
 
-l = process_gzip(get_contentsIndex(get_archi()))
+    top_ten = contador.most_common(10)
+    print "\n Printing output: \n"
+    no = 1
+    for pepe in top_ten:
+        print str(no) + " {0[0]} {0[1]}".format(pepe)
+        no = no + 1
 
-sort_list(l)
 
+def main():
+    l = process_gzip(get_contentsIndex(get_archi()))
+
+    sort_list(l)
+
+if __name__ == "__main__":
+    main()

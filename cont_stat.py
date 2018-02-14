5bb7da797ed7ec514e4651f7a04e94167c3c1eb9
lobo031415 <lobo031415@mail.com>
2018-02-14 16:12:49 +0000

    basez gzip process

--- cont_stat.py
+++ cont_stat.py
@@ -2,6 +2,7 @@
 
 import sys
 import requests
+import gzip
 
 def get_archi():
   if len(sys.argv) == 2:
@@ -18,12 +19,25 @@ def get_archi():
   
     if  sys.argv[1] in opts_archi:
       archi = sys.argv[1]
-      print "Architecture set to", archi
+      return archi
     else:
       print "Argument not in:", opts_archi 
   else:
     print "Nope"
     sys.exit(1)
 
-get_archi()
+
+def get_contentsIndex(arc):
+  cont_url = "http://ftp.uk.debian.org/debian/dists/stable/main/Contents-"+ arc + ".gz"
+  cont_get = requests.get(cont_url)
+  file_archi =cont_url.split('/')[-1]
+  with open(file_archi, "wb") as f: f.write(cont_get.content)
+  return file_archi
+
+def process_gzip(archivo):
+  with gzip.open(archivo, "rb") as gz: gz_content = gz.read()
+  return gz_content
+
+print process_gzip(get_contentsIndex(get_archi()))
+
 
